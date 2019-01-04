be00711050cec3700c196fa49b75de9aac316683
ananasn <ananasn94@gmail.com>
2019-01-04 07:11:53 +0300

    Remove ws support

--- server.py
+++ server.py
@@ -1,48 +1,29 @@
 # --*-- coding:utf-8 --*--
 
+import os
+
+import tornado.gen
+import tornado.ioloop
+import tornado.template
 import tornado.web
 import tornado.websocket
-import tornado.template
-import tornado.ioloop
-import tornado.gen
-import os
 
 PORT = 8181
 HOST = "127.0.0.1"
 
+
 class Handler(tornado.web.RequestHandler):
-	def get(self):
-		self.render(os.path.join(os.path.dirname(__file__), "index.html"))
-
-
-class HTTPHandler(tornado.web.RequestHandler):
-	def get(self):
-		loader = tornado.template.Loader(".")
-		self.write(loader.load(os.path.join(os.path.dirname(__file__),
-            "index.html")).generate(host=HOST, port=PORT))
-
-class WebSocketsHandler(tornado.websocket.WebSocketHandler):
-	clients = []
-	
-	def open(self):
-		print("socket opened")
-
-	def on_message(self, m):
-		print("get message")
-        
-
-	def on_close(self):
-		print("socket closed")
+    def get(self):
+        self.render(os.path.join(os.path.dirname(__file__), "index.html"))
 
 
 if __name__ == "__main__":
-	settings = {"static_path": os.path.join(os.path.dirname(__file__), "static")}
-	app = tornado.web.Application([
-		(r"/", HTTPHandler),
-		(r"/ws", WebSocketsHandler),
-		(r"/(.*)", tornado.web.StaticFileHandler, dict(path=settings["static_path"])),
-	], **settings)
-	app.listen(PORT)
-	
-	main_loop = tornado.ioloop.IOLoop.instance()
-	main_loop.start()
+    settings = {"static_path": os.path.join(os.path.dirname(__file__), "static")}
+    app = tornado.web.Application([
+        (r"/", Handler),
+        (r"/(.*)", tornado.web.StaticFileHandler, dict(path=settings["static_path"])),
+    ], **settings)
+    app.listen(PORT)
+
+    main_loop = tornado.ioloop.IOLoop.instance()
+    main_loop.start()
