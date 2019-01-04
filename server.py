82ae5494d765f05b92f8d08253bc72cc8195fb11
ananasn <ananasn94@gmail.com>
2019-01-04 07:05:47 +0300

    Commit

--- server.py
+++ server.py
@@ -12,15 +12,14 @@ HOST = "127.0.0.1"
 
 class Handler(tornado.web.RequestHandler):
 	def get(self):
-		self.render(os.path.join(os.path.dirname(__file__),
-								 "index.html"))
+		self.render(os.path.join(os.path.dirname(__file__), "index.html"))
 
 
 class HTTPHandler(tornado.web.RequestHandler):
 	def get(self):
 		loader = tornado.template.Loader(".")
 		self.write(loader.load(os.path.join(os.path.dirname(__file__),
-											"index.html")).generate(host=HOST, port=PORT))
+            "index.html")).generate(host=HOST, port=PORT))
 
 class WebSocketsHandler(tornado.websocket.WebSocketHandler):
 	clients = []
@@ -44,7 +43,6 @@ if __name__ == "__main__":
 		(r"/(.*)", tornado.web.StaticFileHandler, dict(path=settings["static_path"])),
 	], **settings)
 	app.listen(PORT)
-	
 	
 	main_loop = tornado.ioloop.IOLoop.instance()
 	main_loop.start()
